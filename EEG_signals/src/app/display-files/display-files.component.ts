e93de45e87357db4ee489592b49e6413a3d77fe0
Alexa-Iordache <alexa.iordache2013@gmail.com>
2024-05-13 11:30:37 +0300

    added method to transform xml to html

--- EEG_signals/src/app/display-files/display-files.component.ts
+++ EEG_signals/src/app/display-files/display-files.component.ts
@@ -35,7 +35,7 @@ export class DisplayFilesComponent {
     'medical_history',
     'diagnosis',
     'treatment',
-    'eeg_signals'
+    'eeg_signals',
   ];
   dataSource: MatTableDataSource<PatientData> = new MatTableDataSource();
   transformedHtml = '';
@@ -47,46 +47,7 @@ export class DisplayFilesComponent {
 
   ngOnChanges(): void {
     if (this.xmlContent) {
-      const xmlData = this.xmlContent;
-      // Define XSLT file path
-      const xsltPath = 'assets/pacienti.xslt';
-
-      // Load XSLT file
-      if (xsltPath) {
-        const xsltProcessor = new XSLTProcessor();
-        const xsltRequest = new XMLHttpRequest();
-        xsltRequest.open('GET', xsltPath, true);
-        xsltRequest.onreadystatechange = () => {
-          if (xsltRequest.readyState === XMLHttpRequest.DONE) {
-            if (xsltRequest.status === 200) {
-              const xsltDocument = xsltRequest.responseXML;
-              if (xsltDocument) {
-                xsltProcessor.importStylesheet(xsltDocument);
-
-                // Perform XSLT transformation
-                const xmlDoc = new DOMParser().parseFromString(
-                  xmlData,
-                  'text/xml'
-                );
-                const transformedDocument = xsltProcessor.transformToDocument(
-                  xmlDoc
-                );
-                const serializer = new XMLSerializer();
-                this.transformedHtml = serializer.serializeToString(
-                  transformedDocument
-                );
-              } else {
-                console.error('Failed to load XSLT document');
-              }
-            } else {
-              console.error('Failed to load XSLT file');
-            }
-          }
-        };
-        xsltRequest.send();
-      } else {
-        console.error('XSLT file path is undefined');
-      }
+      this.transformXmlToHtml(this.xmlContent);
     }
 
     if (this.jsonContent) {
@@ -94,30 +55,30 @@ export class DisplayFilesComponent {
     }
   }
 
-  private parseXML(xmlString: string): void {
-    xml2js.parseString(xmlString, (err: any, result: any) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      this.patients = result.pacienti.pacient.map(
-        (patient: any, index: number) => ({
-          id: (index + 1).toString(),
-          name: `${patient.nume} ${patient.prenume}`,
-          birthDate: patient.data_nastere,
-          allergies: patient.istoric_medical[0].alergii,
-          chronic_diseases: patient.istoric_medical[0].boli_cronice,
-          surgical_history: patient.istoric_medical[0].antecedente_chirurgicale,
-          diagnosis: patient.diagnostic,
-          treatment: patient.tratament,
-          eeg_date: patient.eeg[0].data_inregistrare,
-          interpretation: patient.eeg[0].interpretare,
-        })
-      );
-      this.dataSource.data = this.patients;
-      console.log(this.dataSource.data)
-    });
-  }
+  // private parseXML(xmlString: string): void {
+  //   xml2js.parseString(xmlString, (err: any, result: any) => {
+  //     if (err) {
+  //       console.error(err);
+  //       return;
+  //     }
+  //     this.patients = result.pacienti.pacient.map(
+  //       (patient: any, index: number) => ({
+  //         id: (index + 1).toString(),
+  //         name: `${patient.nume} ${patient.prenume}`,
+  //         birthDate: patient.data_nastere,
+  //         allergies: patient.istoric_medical[0].alergii,
+  //         chronic_diseases: patient.istoric_medical[0].boli_cronice,
+  //         surgical_history: patient.istoric_medical[0].antecedente_chirurgicale,
+  //         diagnosis: patient.diagnostic,
+  //         treatment: patient.tratament,
+  //         eeg_date: patient.eeg[0].data_inregistrare,
+  //         interpretation: patient.eeg[0].interpretare,
+  //       })
+  //     );
+  //     this.dataSource.data = this.patients;
+  //     console.log(this.dataSource.data);
+  //   });
+  // }
 
   private parseJSON(jsonString: string): void {
     const jsonData = JSON.parse(jsonString);
@@ -135,6 +96,44 @@ export class DisplayFilesComponent {
       interpretation: patient.eeg.interpretare,
     }));
     this.dataSource.data = this.patients;
+  }
+
+  private transformXmlToHtml(xmlString: string): void {
+    const xsltPath = 'assets/pacienti.xslt';
+
+    if (xsltPath) {
+      const xsltProcessor = new XSLTProcessor();
+      const xsltRequest = new XMLHttpRequest();
+      xsltRequest.open('GET', xsltPath, true);
+      xsltRequest.onreadystatechange = () => {
+        if (xsltRequest.readyState === XMLHttpRequest.DONE) {
+          if (xsltRequest.status === 200) {
+            const xsltDocument = xsltRequest.responseXML;
+            if (xsltDocument) {
+              xsltProcessor.importStylesheet(xsltDocument);
+
+              // Perform XSLT transformation
+              const xmlDoc = new DOMParser().parseFromString(
+                xmlString,
+                'text/xml'
+              );
+              const transformedDocument =
+                xsltProcessor.transformToDocument(xmlDoc);
+              const serializer = new XMLSerializer();
+              this.transformedHtml =
+                serializer.serializeToString(transformedDocument);
+            } else {
+              console.error('Failed to load XSLT document');
+            }
+          } else {
+            console.error('Failed to load XSLT file');
+          }
+        }
+      };
+      xsltRequest.send();
+    } else {
+      console.error('XSLT file path is undefined');
+    }
   }
 
   ngAfterViewInit(): void {
